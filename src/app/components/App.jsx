02d41c5c4b7540ac3a24e35e530d36515e574bf1
johrielrsrd <johrielrsrd@gmail.com>
2023-10-23 21:15:00 +0800

    added default items

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -5,7 +5,23 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState([
+    {
+      title: "Default Item 1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque arcu et justo luctus egestas. Donec rutrum tincidunt lorem, at sagittis enim feugiat et.",
+    },
+    {
+      title: "Default Item 2",
+      content:
+        "Donec eu ultricies tellus, et eleifend diam. Vivamus tincidunt, massa eget sollicitudin condimentum, lacus ante efficitur leo, et tincidunt libero ipsum ac mauris.",
+    },
+    {
+      title: "Default Item 3",
+      content:
+        "Vivamus eros sapien, cursus ut euismod non, ultrices at metus. Ut ultrices sem libero. Aliquam auctor quam ut orci mollis, id sollicitudin mi tempor. Vestibulum nec ullamcorper tortor.",
+    },
+  ]);
 
   function addNote(inputNote) {
     setItem((prevValue) => {
